10c99cff7fea66f943d32a3b15b62794d58f78e9
Lucas <lucasmachadotellesdaluz@gmail.com>
2026-06-23 19:46:57 -0300

    Estrutura base e automacao

---
+++ Estudante.java
@@ -0,0 +1,10 @@
+public class Estudante{
+    private String nomeAluno;
+    private int matriculaAluno;
+
+public Estudante(String nomeAluno, int matriculaAluno){
+    this.nomeAluno = nomeAluno;
+    this.matriculaAluno = matriculaAluno;
+}
+
+}
