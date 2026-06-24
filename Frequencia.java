8571bdfdcb1697442d0a816f270a897e1d279efe
Lucas <lucasmachadotellesdaluz@gmail.com>
2026-06-23 21:36:09 -0300

    fix: adicionar validacao financeira ao estudante.

--- Frequencia.java
+++ Frequencia.java
@@ -1,8 +1,10 @@
 public class Frequencia {
     private int faltasAluno;
+    private boolean mensalidadeDia;
 
-    public Frequencia(int faltasAluno){
+    public Frequencia(int faltasAluno, boolean mensalidadeDia){
         this.faltasAluno = faltasAluno;
+        this.mensalidadeDia = mensalidadeDia;
     }
 
     
