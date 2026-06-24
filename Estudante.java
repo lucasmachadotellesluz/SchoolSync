7eea9a8d1a2eb0b15fc98e5dbaf5a3518e8375f2
Lucas <lucasmachadotellesdaluz@gmail.com>
2026-06-23 21:33:39 -0300

    fix: adicionar validacao financeira ao estudante.

--- Estudante.java
+++ Estudante.java
@@ -1,10 +1,12 @@
 public class Estudante{
     private String nomeAluno;
     private int matriculaAluno;
+    private boolean mensalidadeDia;
 
-public Estudante(String nomeAluno, int matriculaAluno){
+public Estudante(String nomeAluno, int matriculaAluno, boolean mensalidadeDia){
     this.nomeAluno = nomeAluno;
     this.matriculaAluno = matriculaAluno;
+    this.mensalidadeDia = mensalidadeDia;
 }
 
 }
